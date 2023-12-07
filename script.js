2ebec9eca2b6784ffd554bf543b23baeb9bdb30f
Marta Cezario <142020079+martacezario@users.noreply.github.com>
2023-12-07 18:07:53 +0000

    -

---
+++ script.js
@@ -0,0 +1,69 @@
+// Mostre no console.log a mensagem "Ano novo está chegando!!"
+console.log("Ano novo está chegando !!!")
+
+// Crie uma variável que receba o nome da cantora Mariah Carey
+let nome = "Mariah Carey"
+
+// Crie uma variável que receba a idade dela
+let idade = 54
+
+// Crie uma variável com o ano atual e subtraia pelo o valor da idade da Mariah Carey
+let anoAtual = 2023
+console.log(anoAtual - idade)
+
+// Crie uma variável para guardar a data de nascimento da Mariah Carey (Simples, é só pegar o ano atual e subtrair pela a idade dela 
+
+let dataDenascimento = 1969
+console.log(anoAtual - dataDenascimento)
+
+// Mostre no console a data de nascimento da Mariah Carey
+console.log(`Mariah Carey nasceu em ${dataDenascimento}`)
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console.log
+let quartaFeira = true
+if(quartaFeira == true){
+    console.log("hoje é quarta-feira!")
+}
+
+// Exiba no console o tipo de dado da váriavel quartaFeira
+console.log(typeof quartaFeira)
+
+// Crie uma condição  SE você for maior ou igual 18 retornará  a mensagem:  Você é maior de idade, poderá curtir o show do Roberto Carlos com participação especial de Mariah Carey. SENÃO for maior ou igual a 18,  retorne a seguinte mensagem:  "Sinto muito, mas assistirá da tv globo"
+
+if(idade >= 18){
+    console.log("Você é maior de idade, poderá curtir o show do Roberto Carlos feat Mariah Carey")
+
+
+} else (idade < 18)
+    console.log("Sinto muito, mas terá que assistir pela tv globo!")
+    
+
+//  Crie uma condição que exiba uma mensagem no console se você fizer aniversário em Agosto OU Dezembro OU JUNHO. Caso seja verdadeira a informação, retorne essa mensagem: Uma ou mais opções estão corretas. O mês escolhido foi o mês tal. Caso a informação seja falsa, retorne a seguinte mensagem:  Algo de errado não está certo, o mês digitado foi o mês tal.
+
+let mesDeAniversario = "agosto"
+if(mesDeAniversario == "agosto") {
+    console.log("miaum")
+} else if (animal2 == "papagaio") {
+    console.log(" olá")
+} else if (animal2 == "pato") {
+    console.log("quaqua")
+} else {
+    console.log ("não adivinhou")
+}
+
+
+
+
+
+    // /*let _contador = 0
+    // while(_contador <100){
+    //     contador += 10 /* posso adicionar quanto eu quiser aqui. para mostrar numeros pares podia add 2 */
+    //     // console.log(_contador)}
+
+    //     let contandoPares = 0
+
+    //     while(contandoPares < 50){
+    //         contandoPares += 2
+    //         console.log(contandoPares)
+
+    //    }
